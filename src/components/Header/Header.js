e1937629b4c8402ec4ec9b1201fa2b479d4e3f98
kamalnah2003 <54796340+kamalnah2003@users.noreply.github.com>
2023-03-01 14:56:23 +0530

    hi

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () =>  (
   <Div1>
     <Link href="/">
       <a style={{ display: "flex", alignItems: "center", color: 'white' }}>
-        <DiCssdeck size="3rem" /> <span>Portfoolio</span>
+        <DiCssdeck size="3rem" /> <span>Portfoolioo</span>
       </a>
     </Link>
   </Div1>
